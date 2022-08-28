cc0b430284982caff6dda01fbdd435cecd200f0d
Lucas Assis Rodrigues <lucas.assis.ro@gmail.com>
2022-08-28 15:21:08 -0400

    update

---
+++ Sources/Scope/Scope+Foundation.swift
@@ -0,0 +1,15 @@
+#if canImport(Foundation)
+    import Foundation
+
+    // MARK: - NSObject + Functional
+
+    extension NSObject: Scope {}
+
+    // MARK: - URL + Functional
+
+    extension URL: Scope {}
+
+    // MARK: - Date + Functional
+
+    extension Date: Scope {}
+#endif
