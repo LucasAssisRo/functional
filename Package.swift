3735c4ff8c162346d10047f4e9359d759324c609
Lucas Assis Rodrigues <lucas.assis.ro@gmail.com>
2022-09-24 03:41:55 -0400

    simplify

--- Package.swift
+++ Package.swift
@@ -1,16 +1,9 @@
-// swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 5.7
 
 import PackageDescription
 
 let package = Package(
     name: "Scope",
-    platforms: [
-        .iOS(.v9),
-        .macOS(.v10_10),
-        .watchOS(.v2),
-        .tvOS(.v9),
-    ],
     products: [
         .library(
             name: "Scope",
