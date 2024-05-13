28e3a145641e673d982813dbba82adbea6500078
Lucas Assis Rodrigues <lucas.assis.ro@gmail.com>
2024-05-12 21:59:39 -0400

    feat: add the Emptiable protocol

---
+++ Sources/Functional/Emptiable.swift
@@ -0,0 +1,18 @@
+//
+//  Emptiable.swift
+//
+//
+//  Created by Lucas Assis Rodrigues on 5/12/24.
+//
+
+// MARK: - Emptiable
+
+public protocol Emptiable {
+    var isEmpty: Bool { get }
+}
+
+extension Emptiable {
+    public var isNotEmpty: Bool {
+        !isEmpty
+    }
+}
---
+++ Sources/FunctionalExtensions/Emptiable/Stdlib+Emptiable.swift
@@ -0,0 +1,32 @@
+//
+//  Stdlib+Emptiable.swift
+//
+//
+//  Created by Lucas Assis Rodrigues on 5/12/24.
+//
+
+import Functional
+
+// MARK: - String + Emptiable
+
+extension String: Emptiable {}
+
+// MARK: - Array + Emptiable
+
+extension Array: Emptiable {}
+
+// MARK: - Set + Emptiable
+
+extension Set: Emptiable {}
+
+// MARK: - Dictionary + Emptiable
+
+extension Dictionary: Emptiable {}
+
+// MARK: - CollectionOfOne + Emptiable
+
+extension CollectionOfOne: Emptiable {}
+
+// MARK: - EmptyCollection + Emptiable
+
+extension EmptyCollection: Emptiable {}
